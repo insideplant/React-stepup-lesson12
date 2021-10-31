3d02eb69350d934b7a1a8a1fe2478920d504d366
insideplant <kami.10pearl@gmail.com>
2021-10-31 16:13:41 +0800

    chakra-ui global-style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import './App.css';
 import { Button,ChakraProvider } from '@chakra-ui/react';
+import theme from "./theme/theme"
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   );
