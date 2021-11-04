6842c39669f3f6eab4c7d3ad857adaeb4de04630
insideplant <kami.10pearl@gmail.com>
2021-11-04 22:36:12 +0900

    change Login-component

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,13 @@
 import { memo,VFC } from "react";
-import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+import { Box,
+         Divider, 
+         Flex, 
+         Heading, 
+         Input, 
+         Stack 
+        } from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() =>{
   return (
@@ -11,9 +19,7 @@ export const Login: VFC = memo(() =>{
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
