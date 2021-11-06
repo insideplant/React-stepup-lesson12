0decdf49f77f044ac85ffc896fdb12b2d1f75149
insideplant <kami.10pearl@gmail.com>
2021-11-06 21:29:30 +0900

    Finish

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -5,12 +5,15 @@ import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() =>{
   const { isOpen, onOpen, onClose} = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
-  console.log(selectedUser)
+  const { loginUser } = useLoginUser();
+  
+  console.log(loginUser)
 
   useEffect(() => getUsers(),[])
 
@@ -39,7 +42,12 @@ export const UserManagement: VFC = memo(() =>{
             ) )}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal 
+        user={selectedUser} 
+        isOpen={isOpen} 
+        isAdmin={ loginUser?.isAdmin }
+        onClose={onClose} 
+      />
     </>
   )
 })
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,10 +4,12 @@ import { useHistory } from "react-router";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser} from "../hooks/useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
+  const { setLoginUser } = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
@@ -17,16 +19,19 @@ export const useAuth = () => {
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        const isAdmin = res.data.id === 10 ? true : false;
+        setLoginUser({...res.data, isAdmin});
         showMessage({ title: "ログインしました", status: "success"})
         history.push("/home");
       } else {
         showMessage({ title: "ユーザーが見つかりません", status: "error"})
         alert("ユーザーが見つかりません");
+        setLoading(false);
       }
     })
     .catch(() => 
       showMessage({ title: "ログインできません", status: "error"}))
-    .finally(() => setLoading(false));
-  },[history, showMessage]);
+      setLoading(false);
+  },[history, showMessage, setLoginUser]);
   return { login, loading }
 }
